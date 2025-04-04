1d6a345f699b5b26e5526fd67daac8fba429be08
Santiago Sanabria <santiago2004_s_b@outlook.com>
2025-04-03 22:51:35 -0600

    feat: ConsultarPorAgencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { useState } from 'react';
-import './App.css';
-import { SIDEMENU } from './sideMenu/sideMenu.jsx';
-import { MAINDASHBOARD } from './Content/paginaPrincipal.jsx';
-import { ADDACCIDENTS } from './Content/agregarSiniestros.jsx';
-import { GARAGE } from './Content/talleresMecanicos.jsx';
-import { CONSULTARSINIESTROS } from './Content/consultarSiniestros.jsx';
+import { useState } from "react";
+import "./App.css";
+import { SIDEMENU } from "./sideMenu/sideMenu.jsx";
+import { MAINDASHBOARD } from "./Content/paginaPrincipal.jsx";
+import { ADDACCIDENTS } from "./Content/agregarSiniestros.jsx";
+import { GARAGE } from "./Content/talleresMecanicos.jsx";
+import { CONSULTARSINIESTROS } from "./Content/consultarSiniestros.jsx";
+import { CONSULTARPORAGENCIA } from "./Content/consultarPorAgencia.jsx";
 
 function App() {
-  const [active, setActive] = useState('MAINDASHBOARD');
+  const [active, setActive] = useState("MAINDASHBOARD");
 
   return (
     <div className="flex h-screen w-screen">
       <section className="h-full w-[350px] bg-[#013462] transform hidden sm:block transition-all duration-1000">
         <SIDEMENU setActive={setActive} />
       </section>
-      
+
       <section className="h-full bg-[#00111F] w-full transition-all duration-1000">
-        {active === 'MAINDASHBOARD' && <MAINDASHBOARD />}
-        
-        {active === 'GARAGE' && <GARAGE />}
-        {active === 'CONSULTARSINIESTROS' && <CONSULTARSINIESTROS />}
-        {active === 'ADDACCIDENTS' && <ADDACCIDENTS />}
+        {active === "MAINDASHBOARD" && <MAINDASHBOARD />}
+        {active === "GARAGE" && <GARAGE />}
+        {active === "CONSULTARSINIESTROS" && <CONSULTARSINIESTROS />}
+        {active === "ADDACCIDENTS" && <ADDACCIDENTS />}
+        {active === "CONSULTARPORAGENCIA" && <CONSULTARPORAGENCIA />}
       </section>
     </div>
   );
 }
 
 export default App;
-
-
--- src/sideMenu/sideMenuComponents/buttons.jsx
+++ src/sideMenu/sideMenuComponents/buttons.jsx
@@ -1,40 +1,44 @@
-import icons from '../../assets/icons.jsx';
+import icons from "../../assets/icons.jsx";
 
 function BTN({ icon, text, arrow, onClick }) {
-    return (
-        <button id='btn-menu' onClick={onClick} className="flex justify-between items-center w-full p-2 text-white">
-            {icon}
-            <span id='a-btn'>{text}</span>
-            {arrow}
-        </button>
-    );
+  return (
+    <button
+      id="btn-menu"
+      onClick={onClick}
+      className="flex justify-between items-center w-full p-2 text-white"
+    >
+      {icon}
+      <span id="a-btn">{text}</span>
+      {arrow}
+    </button>
+  );
 }
 
 export default function Button({ setActive }) {
-    function handleClick(sectionClicked) {
-        let active = 'MAINDASHBOARD';
+  function handleClick(sectionClicked) {
+    let active = "MAINDASHBOARD";
 
-        if (sectionClicked === 'Mecanico') {
-            active = 'GARAGE';
-        } else if (sectionClicked === 'Agregar') {
-            active = 'ADDACCIDENTS';
-        }
-        else if (sectionClicked === 'Consultar') {
-            active = 'CONSULTARSINIESTROS';
-        }
-
-        setActive(active);
+    if (sectionClicked === "Mecanico") {
+      active = "GARAGE";
+    } else if (sectionClicked === "Agregar") {
+      active = "ADDACCIDENTS";
+    } else if (sectionClicked === "Consultar") {
+      active = "CONSULTARSINIESTROS";
+    } else if (sectionClicked === "Agencia") {
+      active = "CONSULTARPORAGENCIA";
     }
+    setActive(active);
+  }
 
-    return (
-        <menu className="flex flex-col items-center justify-center gap-[15px] text-[14px] w-full">
-            <BTN onClick={() => handleClick("Principal")} {...icons.Principal} />
-            <BTN onClick={() => handleClick("Mecanico")} {...icons.Mecanico} />
-            <BTN onClick={() => handleClick("Ubicacion")} {...icons.Ubicacion} />
-            <BTN onClick={() => handleClick("Agregar")} {...icons.Agregar} />
-            <BTN onClick={() => handleClick("Consultar")} {...icons.Consultar} />
-            <BTN onClick={() => handleClick("Agencia")} {...icons.Agencia} />
-            <BTN onClick={() => handleClick("Historial")} {...icons.Historial} />
-        </menu>
-    );
+  return (
+    <menu className="flex flex-col items-center justify-center gap-[15px] text-[14px] w-full">
+      <BTN onClick={() => handleClick("Principal")} {...icons.Principal} />
+      <BTN onClick={() => handleClick("Mecanico")} {...icons.Mecanico} />
+      <BTN onClick={() => handleClick("Ubicacion")} {...icons.Ubicacion} />
+      <BTN onClick={() => handleClick("Agregar")} {...icons.Agregar} />
+      <BTN onClick={() => handleClick("Consultar")} {...icons.Consultar} />
+      <BTN onClick={() => handleClick("Agencia")} {...icons.Agencia} />
+      <BTN onClick={() => handleClick("Historial")} {...icons.Historial} />
+    </menu>
+  );
 }
