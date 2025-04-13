5292a3a8e0cd61d62f3dd846b6eaf0ddc2ae24af
luismdg <luism.delgadillo1234@gmail.com>
2025-04-13 00:53:49 -0600

    Talleres MEcanicos Diseno

--- src/Content/GARAGE/talleresMecanicos.jsx
+++ src/Content/GARAGE/talleresMecanicos.jsx
@@ -23,13 +23,21 @@ export function GARAGE() {
                     <FilterButtons/>
                 </div>
 
-                <div>
-                  <span>Estado: NUEVO LEON</span>
-                  <span>Estado: MONTERREY</span>
-                  <span>Estado: 64280</span>
-                  <span>Estado: AUTOS</span>
-                  <span>Estado: ACTIVO</span>
-                  <span>Estado: SI</span>
+                <div id="filteredData" className="grid grid-cols-[5px_1fr_5px_1fr] gap-4">
+
+                  <div className="bg-[#009AD4] h-full w-[2px]"></div>
+                  <div className="flex flex-col gap-1">
+                    <span>Estado: NUEVO LEON</span>
+                    <span>Municipio: MONTERREY</span>
+                    <span>Codigo Postal: 64280</span>
+                  </div>
+
+                  <div className="bg-[#009AD4] h-full w-[2px]"></div>
+                  <div className="flex flex-col gap-1">
+                    <span>Tipo Unidad: AUTOS</span>
+                    <span>Inscrito a CESVI: ACTIVO</span>
+                    <span>Taller Exclusivo: SI</span>
+                  </div>
                 </div>
 
                 <div id="dashboardContainer" className="bg-[#D9D9D9] w-full h-screen">
